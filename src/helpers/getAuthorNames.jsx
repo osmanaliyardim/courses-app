d710e74962e3ebe139a4d42efdbe508f5ef9a3c4
Osman Ali Yardım <osmanaliyardim@gmail.com>
2023-03-02 13:03:08 +0300

    REFACTOR: Function naming

--- src/helpers/getAuthorNames.jsx
+++ src/helpers/getAuthorNames.jsx
@@ -1,28 +1,18 @@
-//ToDo: will be fixed
-//import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-//const getAuthorNames = (authorParams) => {
-//   const authors = useSelector((state) => state.courses.authors);
-//   const authorNames = [];
-
-//   authorParams.forEach((paramElement) => {
-//     authors.forEach((author) => {
-//       if (paramElement === author.id) {
-//         authorNames.push(author.name);
-//       }
-//     });
-//   });
-
-//   return authorNames;
-// };
-
-//Mock
-const getAuthorNames = (authorParams) => {
+const GetAuthorNames = (authorParams) => {
+  const authors = useSelector((state) => state.courses.authors);
   const authorNames = [];
 
-  authorNames.push('osmanaliyardim');
+  authorParams.forEach((paramElement) => {
+    authors.forEach((author) => {
+      if (paramElement === author.id) {
+        authorNames.push(author.name);
+      }
+    });
+  });
 
   return authorNames;
 };
 
-export default getAuthorNames;
+export default GetAuthorNames;
